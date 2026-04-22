db9135c20434e0f724915fc15fab82dbea196de4
NikitaPerzhinsky <nperzhinskiy@mail.ru>
2026-04-22 23:04:22 +0300

    Delete def sum_of_two(nums, target).py

--- def sum_of_two(nums, target).py
+++
@@ -1,8 +0,0 @@
-def sum_of_two(nums, target)
-    n = len(nums)
-    for i in range(n)
-        for j in range(i + 1, n)
-        if nums[i] + nums[j] == target
-            return [i, j]
-    return [] 
-           
